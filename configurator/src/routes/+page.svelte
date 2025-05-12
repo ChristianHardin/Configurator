43ebe60f38719f2ccd342c7fe28243b304b2fe2f
Christian Hardin <contact@christianhardin.com>
2025-05-11 21:18:56 -0400

    feat: added darkmode

--- configurator/src/routes/+page.svelte
+++ configurator/src/routes/+page.svelte
@@ -1,10 +1,24 @@
 <script>
-	import { Alert } from "flowbite-svelte";
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+	} from "flowbite-svelte";
+	import { DarkMode } from "flowbite-svelte";
+	import logo from "$lib/assets/logo.png";
 </script>
 
-<div class="p-8">
-	<Alert>
-		<span class="font-medium">Info alert!</span>
-		Change a few things up and try submitting again.
-	</Alert>
-</div>
+<Navbar>
+	<NavBrand href="/">
+		<img src={logo} class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+		<span
+			class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+			>Anco Configurator</span
+		>
+	</NavBrand>
+	<DarkMode
+		class="text-primary-500 dark:text-primary-600 border dark:border-gray-800"
+	/>
+</Navbar>
